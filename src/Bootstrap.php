f2eb3bb6bec956ed751ee2babca2e90be2bd64ad
Karl Zollner <iwbcman@gmail.com>
2019-03-27 21:22:54 -0400

    routing

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 namespace App;
+use Http\HttpRequest;
+use Http\HttpResponse;
 require __DIR__ . '/../vendor/autoload.php';
 
 error_reporting(E_ALL);
@@ -16,5 +18,43 @@ if ($environment !== 'production') {
     });
 }
 $whoops->register();
-throw new \Exception;
-
+//throw new \Exception;
+$request = new HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
+$response = new HttpResponse;
+//$content = '<h1>Hellow World</h1>';
+/**
+$dispatcher = \FastRoute\simpleDispatcher(function (\FastRoute\RouteCollector $r) {
+   $r->addRoute('GET', '/hello-world', function (){
+       echo 'Hello World';
+   });
+});
+ *
+ */
+$routeDefinitionCallback = function (\FastRoute\RouteCollector $r) {
+  $routes = include ('Routes.php');
+  foreach ($routes as $route) {
+      $r->addRoute([0], $route[1], $route[2]);
+  }
+};
+$dispatcher = \FastRoute\simpleDispatcher($routeDefinitionCallback);
+$routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getPath());
+switch ($routeInfo[0]) {
+    case \FastRoute\Dispatcher::NOT_FOUND:
+        $response->setContent('404 - Page not found');
+        $response->getStatusCode(404);
+        break;
+    case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $response->setContent('405 - Method not allowed');
+        $response->getStatusCode(405);
+        break;
+    case \FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        call_user_func($handler, $vars);
+        break;
+}
+//$response->setContent($content);
+foreach ($response->getHeaders() as $header) {
+    header($header, false);
+}
+echo $response->getContent();
---
+++ src/Routes.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    ['GET', '/hello-world', function () {
+        echo 'Hello World';
+    }],
+    ['GET', '/another-route', function () {
+        echo 'This works too';
+    }],
+];
